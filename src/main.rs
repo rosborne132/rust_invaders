6ebfedda80edc402704d0215dcad9573429bf022
rosborne132 <ozborne132@gmail.com>
2021-07-04 16:07:19 -0700

    First commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::error::Error;
+use rusty_audio::Audio;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "assets/sounds/explode.wav");
+    audio.add("lose", "assets/sounds/lose.wav");
+    audio.add("move", "assets/sounds/move.wav");
+    audio.add("pew", "assets/sounds/pew.wav");
+    audio.add("startup", "assets/sounds/startup.wav");
+    audio.add("win", "assets/sounds/win.wav");
+    audio.play("startup");
+
+    // Cleanup
+    audio.wait();
+    Ok(())
+}
